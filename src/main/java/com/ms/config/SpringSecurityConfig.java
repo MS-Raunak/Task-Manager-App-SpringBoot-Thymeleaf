314c8818a4099c2cad9ad8e2bab6df051f163f3c
MS-Raunak <raunakkr.saha@gmail.com>
2024-09-02 02:06:40 +0530

    In-memory Authentication: Spring-Security

--- src/main/java/com/ms/config/SpringSecurityConfig.java
+++ src/main/java/com/ms/config/SpringSecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +25,28 @@ public class SpringSecurityConfig {
 			
 		
 		return http.build();
+	}
+	
+	
+	//In-memory authentication
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder()
+						 .username("user")
+						 .password(passwordEncoder().encode("123"))
+						 .roles("USER")
+						 .build();
+		UserDetails user2 = User.builder()
+				            .username("admin")
+				            .password(passwordEncoder().encode("123"))
+				            .roles("ADMIN")
+				            .build();
+		
+		return new InMemoryUserDetailsManager(user, user2);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
